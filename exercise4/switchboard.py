a23558d7dc1c5b9562b9beae818a9db4866f92b7
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2014-10-14 19:42:07 +0200

    Wrote evaluateBoard

--- exercise4/switchboard.py
+++ exercise4/switchboard.py
@@ -3,18 +3,22 @@ import math, random
 
 class Switchboard(object):
     def __init__(self, m, n, d, w, start, end):
-        # 0 for no wire, 1 for regular wire, 2 for turn
+    # 0 for no wire, 1 for regular wire, 2 for turn
         self.switchboard =  [[0 for x in range(n)] for x in range(m)]
-        self.distance = d
+        self.distanceCost = d
         self.turnCost = w
         self.start = start
         self.end = end
 
-    def generateNeighbours(self):
-        return
-
     def evaluateBoard(self):
-        return
+        price = 0
+        for line in self.switchboard:
+            for val in line:
+                if (val == 1):
+                    price += self.distanceCost
+                elif val == 2:
+                    price += self.turnCost
+        return price
 
     def __str__(self):
         return str(self.switchboard)
